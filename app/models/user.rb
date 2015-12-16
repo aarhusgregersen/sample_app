dacceeef52d3a103dfab96304af3e06e36100b17
Martin Aarhus Gregersen <marting12@live.dk>
2015-12-16 14:39:05 +0100

    Finalizes user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,   foreign_key: "follower_id", 
+                                    class_name: "Relationship",
+                                    dependent: :destroy
+  has_many :passive_relationships,  foreign_key: "followed_id",
+                                    class_name: "Relationship",
+                                    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
 
 	def User.digest(string)
@@ -36,6 +45,24 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
